6be7f8b26a8f53a4228e8a9c52e30fb68190a1b9
Lilico1921 <125165369+Lilico1921@users.noreply.github.com>
2024-01-05 22:15:00 -0300

    Add files via upload

---
+++ fulana de tal/quiz/questions.js
@@ -0,0 +1,83 @@
+export default [
+    {
+      question: "Em que ano foi lançado o filme Star Wars:",
+      answers: [
+        { option: "1987", correct: false },
+        { option: "1976", correct: false },
+        { option: "1977", correct: true },
+        { option: "1974", correct: false },
+      ],
+    },
+    {
+      question: "Batman teve vários robins ao longo da história.Qual o nome do primeiro robin?",
+      answers: [
+        { option: "Marc Grayson", correct: false },
+        { option: "Jason Toddy", correct: false },
+        { option: "Damian Wayne", correct: false },
+        { option: "Dick Grayson", correct: true },
+      ],
+    },
+    {
+      question: "Pq taylor criou o taylor’s version?",
+      answers: [
+        { option: "Pq a Taylor achou que deveria ter seu nome nas músicas pois as músicas eram dela e como ela que cantava as músicas ela botou o taylor’s version para mostrar que era a versão dela", correct: false },
+        { option: "Pq taylor fazia parte de uma gravadora que após uma treta não queria devolver as músicas para a taylor ent taylor começou a refazer as músicas", correct: true },
+        { option: "Pq foi ela quem escreveu as letras", correct: false },
+        { option: "Pq ela achou bonito", correct: false },
+      ],
+    },
+    {
+      question: "Qual a menor nota que se pode tirar em Hogwarts?",
+      answers: [
+        { option: "T de Trasgo", correct: true },
+        { option: "P de Péssimo", correct: false },
+        { option: "D de Deplorável", correct: false },
+        { option: "T de Trouxa", correct: false },
+      ],
+    },
+    {
+      question: "Qual é o maior goleador da história do Internacional? E quantos gols foram feitos por esse ex-jogador?",
+      answers: [
+        { option: "D'Alessandro. 79 Gols", correct: false },
+        { option: "Fernandão, 293 Gols", correct: false },
+        { option: "Falcão, 342 Gols", correct: false },
+        { option: "Carlitos, 325 Gols", correct: true },
+      ],
+    },
+    {
+      question: "Quando a gente se atrasou pro filme da taylor, pra qual filme a gente resgatou ingresso pra poder entrar na sala?",
+      answers: [
+        { option: "FNAF", correct: false },
+        { option: "Não Abra", correct: true },
+        { option: "Trolls 3", correct: false },
+        { option: "Assassinos da Lua das Flores", correct: false },
+      ],
+    },
+    {
+      question: "Quais os dois times que a Fulana torce na NFL?",
+      answers: [
+        { option: "Denver Broncos e Kansas City Chiefs", correct: false },
+        { option: "Minnesota Vikings e Green Bay Packers", correct: false },
+        { option: "Cincinnati Bengals e Seattle Seahawks", correct: true },
+        { option: "San Francisco 49ers e Philadelphia Eagles", correct: false },
+      ],
+    },
+    {
+      question: "De qual casa de Hogwarts a Fulana é e de qual eu achei que ela fosse?",
+      answers: [
+        { option: "Grifinória mas achei que fosse da Corvinal", correct: false },
+        { option: "Sonserina mas achei que fosse da Corvinal", correct: true },
+        { option: "Corvinal mas achei que fosse da Lufa-Lufa", correct: false },
+        { option: "Lufa-Lufa mas achei que fosse da Grifinória", correct: false },
+      ],
+    },
+    {
+      question: "Qual música tava tocando quando a gente chegou na sala de cinema?",
+      answers: [
+        { option: "Willow", correct: false },
+        { option: "The man", correct: false },
+        { option: "Fearless", correct: true },
+        { option: "Love Story", correct: false },
+      ],
+    },
+  ];
---
+++ fulana de tal/quiz/script.js
@@ -0,0 +1,81 @@
+window.alert("Are You a Karma? Because Karma is a Queen");
+window.alert("Bem vindo ao Quiz para conquistar a Fulana! Acha que ta preparado?");
+
+//musica lwymmd
+function playAudio() {
+  let x = document.getElementById("myAudio");
+  x.play();
+}
+function pauseAudio() {
+  let x = document.getElementById("myAudio");
+  x.pause();
+}       
+
+//video da edit
+
+
+const question = document.querySelector(".question");
+const answers = document.querySelector(".answers");
+const spnQtd = document.querySelector(".spnQtd");
+const textFinish = document.querySelector(".finish span");
+const content = document.querySelector(".content");
+const contentFinish = document.querySelector(".finish");
+const btnRestart = document.querySelector(".finish button");
+
+import questions from "./questions.js";
+
+let currentIndex = 0;
+let questionsCorrect = 0;
+
+btnRestart.onclick = () => {
+  content.style.display = "flex";
+  contentFinish.style.display = "none";
+
+  currentIndex = 0;
+  questionsCorrect = 0;
+  loadQuestion();
+};
+
+function nextQuestion(e) {
+  if (e.target.getAttribute("data-correct") === "true") {
+    questionsCorrect++;
+  }
+
+  if (currentIndex < questions.length - 1) {
+    currentIndex++;
+    loadQuestion();
+  } else {
+    finish();
+  }
+}
+
+function finish() {
+  textFinish.innerHTML = `você acertou ${questionsCorrect} de ${questions.length}`;
+  content.style.display = "none";
+  contentFinish.style.display = "flex";
+}
+
+function loadQuestion() {
+  spnQtd.innerHTML = `${currentIndex + 1}/${questions.length}`;
+  const item = questions[currentIndex];
+  answers.innerHTML = "";
+  question.innerHTML = item.question;
+
+  item.answers.forEach((answer) => {
+    const div = document.createElement("div");
+
+    div.innerHTML = `
+    <button class="answer" data-correct="${answer.correct}">
+      ${answer.option}
+    </button>
+    `;
+
+    answers.appendChild(div);
+  });
+
+  document.querySelectorAll(".answer").forEach((item) => {
+    item.addEventListener("click", nextQuestion);
+  });
+}
+
+loadQuestion();
